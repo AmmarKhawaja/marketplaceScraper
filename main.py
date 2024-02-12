8edabd66b66a1de05936ae74b4da511540a6cfb8
Ammar Khawaja <ammarkhawaja01@gmail.com>
2024-02-11 22:45:36 -0500

    Major changes

--- main.py
+++ main.py
@@ -14,17 +14,20 @@ def get_raw_text(url='test'):
         'https': 'http://' + USER + ':' + PASS + '@unblock.oxylabs.io:60000',
     }
     headers_list = [{ 
-        "upgrade-insecure-requests": "1",
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Windows; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36",
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-        "sec-ch-ua": "\".Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"103\", \"Chromium\";v=\"103\"",
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": "\"Windows\"",
-        "sec-fetch-site": "none",
-        "sec-fetch-mod": "",
-        "sec-fetch-user": "?1",
-        "accept-encoding": "gzip, deflate, br",
-        "accept-language": "bg-BG,bg;q=0.9,en-US;q=0.8,en;q=0.7"
+        'authority': 'in.hotjar.com',
+        'method': 'POST',
+        'path': '/api/v2/client/sites/2829708/visit-data?sv=7',
+        'scheme': 'https',
+        'accept': '*/*',
+        'accept-encoding': 'gzip, deflate, br',
+        'accept-language': 'en-US,en;q=0.9,it;q=0.8,es;q=0.7',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101", "Microsoft Edge";v="101"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': "macOS",
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'cross-site',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36 Edg/101.0.1210.47',
     }] 
     return requests.request('GET', url, headers=random.choice(headers_list), verify=False, proxies=proxies).text
 
@@ -67,9 +70,9 @@ for product in products:
     ctr += 1
     if prices:
         print(str((ctr / len(l_lines)) * 100)[0:5])
-    print(date.datetime())
+    print(datetime.now())
+    print(prices)
     prices = []
-    print("-")
     for location in locations.keys():
         url = 'https://www.facebook.com/marketplace/' + locations[location].strip() + '/search?query=' + product.replace(' ', '%20')
     
